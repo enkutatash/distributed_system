83b878342477a07dffb434f4eeec59355c465918
enkutatash <enkutatasheshetu96@gmail.com>
2025-12-31 22:00:25 +0300

    static file for payment

--- payment_service/payment_service/payment/urls.py
+++ payment_service/payment_service/payment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PaymentCreateView, StripeWebhookView
+from .views import PaymentCreateView, StripeWebhookView, payment_success, payment_cancel
 
 urlpatterns = [
     path('api/v1/payments/', PaymentCreateView.as_view(), name='payment-create'),
     path('api/v1/payments/webhook/', StripeWebhookView.as_view(), name='payment-webhook'),
+    path('payment/success/', payment_success, name='payment-success'),
+    path('payment/cancel/', payment_cancel, name='payment-cancel'),
 ]
